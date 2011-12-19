ca48c8fab038540eec65f242c022b4c0eb443562
Anatoly Kudinov <zz@rialabs.org>
2011-12-19 05:49:23 +0400

    basic setup script, header for __init__.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Fullboar Creative, Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from distutils.core import setup
+
+setup(name='pyvotal',
+    version = '0.1',
+    description = 'pivotal tracker api client',
+    author = 'Anatoly Kudinov',
+    author_email = 'zz@rialabs.org',
+    url = 'https://github.com/zerko/pyvotal',
+    packages = ['pyvotal',],
+    requires = ['restclient', 'fluentxml']
+)
