09b45013df2c49e17d24ea573c40a52f025af5fd
Michael Bunsen <notbot@gmail.com>
2012-03-27 11:07:59 -0700

    Set explicit dictshield version in package requirements. Resolves issue #3.

--- setup.py
+++ setup.py
@@ -24,5 +24,5 @@ setup(name='pyvotal',
     author_email = 'zz@rialabs.org',
     url = 'https://github.com/fullboar/pyvotal',
     packages = ['pyvotal',],
-    install_requires = ['requests', 'dictshield', 'python-dateutil<2']
+    install_requires = ['requests', 'dictshield==0.3.6', 'python-dateutil<2']
 )
